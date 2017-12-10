09aa5408284ac6cf7a6368ece67e6f8b46119ed7
cnr <ConnorNJames@gmail.com>
2017-12-10 03:24:24 -0600

    Replace Rotation -- [a] -> Writer (Sum Int) [a]

--- day10/Day10.hs
+++ day10/Day10.hs
@@ -1,10 +1,10 @@
 #!/usr/bin/env stack
 -- stack runghc
 
+import Control.Monad.Writer
 import Data.Bits (xor)
 import Data.Char (ord)
 import Data.List.Split (chunksOf, splitOn)
-import Data.Monoid ((<>))
 import Text.Printf (printf)
 
 main :: IO ()
@@ -18,43 +18,31 @@ main = do
     print $ part2 lengths2
 
 part1 :: [Int] -> Int
-part1 = combine . runRounds
-    where
-    combine   = product . take 2
-    runRounds = runRotation [0..255] . mconcat . zipWith step [0..]
+part1 = product . take 2 . runRound
 
 part2 :: [Int] -> String
-part2 = combine . runRounds
-    where
-    combine   = concatMap (showHexByte . foldr xor 0) . chunksOf 16
-    runRounds = runRotation [0..255] . mconcat . zipWith step [0..] . concat . replicate 64
+part2 = concatMap (showHexByte . foldr xor 0) . chunksOf 16 . runRound . concat . replicate 64
 
-step :: Int -> Int -> Rotation a
-step skip len = liftR (\xs -> reverse (take len xs) ++ drop len xs) <> rotateR (len + skip)
+runRound :: [Int] -> [Int]
+runRound xs = runRotation (foldl1 (>=>) (zipWith step [0..] xs) [0..255])
+
+step :: Int -> Int -> [a] -> Rotation a
+step skip len xs = rotateR (len + skip) (reverse (take len xs) ++ drop len xs)
 
 -- The 'cute' solution
 
-newtype Rotation a = Rotation { performRotation :: [a] -> (Int, [a]) }
+type Rotation a = Writer (Sum Int) [a]
 
-runRotation :: [a] -> Rotation a -> [a]
-runRotation xs = uncurry fixRotation . (`performRotation` xs)
+runRotation :: Rotation a -> [a]
+runRotation = uncurry fixRotation . runWriter
     where
-    fixRotation rot = rotate (length xs - (rot `mod` length xs))
-
-instance Monoid (Rotation a) where
-    mempty = Rotation (\xs -> (0, xs))
-    mappend (Rotation first) (Rotation second) = Rotation $ \xs -> let (rot1, xs')  = first xs
-                                                                       (rot2, xs'') = second xs'
-                                                                    in (rot1 + rot2, xs'')
+    fixRotation xs (Sum rot) = rotate (length xs - (rot `mod` length xs)) xs
 
 showHexByte :: Int -> String
 showHexByte = printf "%02x"
 
-rotateR :: Int -> Rotation a
-rotateR n = Rotation (\xs -> (n, rotate n xs))
+rotateR :: Int -> [a] -> Rotation a
+rotateR n xs = rotate n xs <$ tell (Sum n)
 
 rotate :: Int -> [a] -> [a]
 rotate n xs = take (length xs) (drop n (cycle xs))
-
-liftR :: ([a] -> [a]) -> Rotation a
-liftR f = Rotation (\xs -> (0, f xs))
